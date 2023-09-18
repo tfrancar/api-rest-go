69e10de3a74232cb62a7a27fcd5136188d78d426
Thiago <tfrancar@gmail.com>
2023-09-17 23:00:36 -0300

    Mocando models.Personalidades

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/tfrancar/api-rest-go/models"
 	"github.com/tfrancar/api-rest-go/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor rest com go")
 	routes.HandleRequest()
 }
