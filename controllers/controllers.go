9ab10b9c1100e2b0b8234d2a75844f002201c9b5
Thiago <tfrancar@gmail.com>
2023-09-21 22:18:48 -0300

    Inclusão do content-type

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -16,12 +16,14 @@ func Home(w http.ResponseWriter, _ *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	var array_personalidades []models.Personalidade
 	database.DB.Find(&array_personalidades)
 	json.NewEncoder(w).Encode(array_personalidades)
 }
 
 func RetornaUmaPersonaldiade(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	vars := mux.Vars(r)
 	id := vars["id"]
 	var personalidade models.Personalidade
@@ -31,6 +33,7 @@ func RetornaUmaPersonaldiade(w http.ResponseWriter, r *http.Request) {
 }
 
 func CriarUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	var novaPersonalidade models.Personalidade
 	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
 	database.DB.Create(&novaPersonalidade)
@@ -39,6 +42,7 @@ func CriarUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	vars := mux.Vars(r)
 	id := vars["id"]
 	var personalidade models.Personalidade
@@ -46,7 +50,8 @@ func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(personalidade)
 }
 
-func EditaPersonalidade(w http.ResponseWriter, r *http.Request)  {
+func EditaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	vars := mux.Vars(r)
 	id := vars["id"]
 	var personalidade models.Personalidade
@@ -54,5 +59,5 @@ func EditaPersonalidade(w http.ResponseWriter, r *http.Request)  {
 	json.NewDecoder(r.Body).Decode(&personalidade)
 	database.DB.Save(&personalidade)
 	json.NewEncoder(w).Encode(personalidade)
-	
+
 }
