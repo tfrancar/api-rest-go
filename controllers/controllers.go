7299291cdb320458b3aab09b48c509b0fd7b701a
Thiago <tfrancar@gmail.com>
2023-09-21 21:02:30 -0300

    Incluindo o find para personalidade e array de personalidade

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,28 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/tfrancar/api-rest-go/database"
 	"github.com/tfrancar/api-rest-go/models"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+// underscore usado para suprimir o r
+func Home(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
 
-func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+func TodasPersonalidades(w http.ResponseWriter, _ *http.Request) {
+	var array_personalidades []models.Personalidade
+	database.DB.Find(&array_personalidades)
+	json.NewEncoder(w).Encode(array_personalidades)
 }
 
 func RetornaUmaPersonaldiade(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-
-	for _, personalidade := range models.Personalidades {
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
 
 }
